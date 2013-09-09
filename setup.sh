6702df93989e8fdd4a43090bc904068bf7fa2f93
zzMOM <wuwei1107@gmail.com>
2013-09-09 14:34:58 -0700

    setup.sh for EC2

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2
+# for headless setup.
+
+# Install Github
+sudo apt-get install -y git-core
+
+# Install heroku 
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# Install node.js
+# Ubuntu, Mint, Elementary OS
+# https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
